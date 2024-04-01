1e0b5107733726d611c56f5c9f7f023f137a720c
jinho9482 <157558300+jinho9482@users.noreply.github.com>
2024-04-01 23:03:21 +0900

    See os information using os module

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-// path module
+// Node 를 통해 os, file system, network 의 정보도 얻을 수 있다.
+// 기존의 js는 browser에서 움직였기 때문에 window(document)의 정보만 얻을 수 있었다.
 
-const path = require("path");
+const os = require("os");
 
-var pathObj = path.parse(__filename); // returns an object whose properties represent significant elements of the __filename
+let totalMemory = os.totalmem();
+let freeMemory = os.freemem();
 
-console.log(pathObj);
-
-// returns:
-// {
-//     root: 'C:\\',
-//     dir: 'C:\\nodejs-study',
-//     base: 'app.js',
-//     ext: '.js',
-//     name: 'app'
-//   }
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
